b07358af8992ab2909269e87a37cee7d25021696
Satyaprakash <161062991+satyam5782@users.noreply.github.com>
2024-06-18 02:04:25 +0530

    Add files via upload

---
+++ hangman_words.py
@@ -0,0 +1,13 @@
+# Words for the Hangman Game
+word_list = [
+    "elephant", "computer", "sunshine", "keyboard", "Butterfly",
+    "chocolate", "adventure", "Firefly", "cucumber", "Whiskey",
+    "Parrot", "Mystery", "Rainbow", "Universe", "synergy",
+    "elephant", "symphony", "kangaroo", "Backpack", "Pumpkin",
+    "Jigsaw", "galaxy", "Pizza", "Harmony", "Dolphin",
+    "Pajamas", "espresso", "Astronaut", "Velocity", "Zephyr",
+    "Midnight", "squirrel", "Pavilion", "elephant", "Jamboree",
+    "kaleidoscope", "Marzipan", "serenade", "Velocity", "Paradox",
+    "cacophony", "Quasar", "Tiramisu", "Raspberry", "Plenitude",
+    "Whimsical", "Bamboo", "Zucchini", "kaleidoscope", "Velociraptor"
+]
